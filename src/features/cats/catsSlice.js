50738f5786b1d4c6715c930c73936fbddf629559
Win Feigle <win.feigle@gmail.com>
2023-02-06 09:13:48 -0700

    commit tree 3f3446c6a5ff6f24da52727d254fc15bc21b3710

--- src/features/cats/catsSlice.js
+++ src/features/cats/catsSlice.js
@@ -1,34 +1,13 @@
 // Action Creators
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 // async actions
-export function fetchCats() {
-  return function (dispatch) {
-    dispatch({ type: "cats/fetchCats/pending" });
-    fetch("https://learn-co-curriculum.github.io/cat-api/cats.json")
-      .then((response) => response.json())
-      .then((data) => {
-        dispatch({
-          type: "cats/fetchCats/fulfilled",
-          payload: data.images,
-        });
-      });
-  };
-}
-
-// sync actions added for demo purposes
-export function catAdded(newCat) {
-  return {
-    type: "cats/catAdded",
-    payload: newCat,
-  };
-}
-
-export function catUpdated(updatedCat) {
-  return {
-    type: "cats/catUpdated",
-    payload: updatedCat,
-  };
-}
+export const fetchCats = createAsyncThunk("cats/fetchCats", () => {
+  // return a Promise containing the data we want
+  return fetch("https://learn-co-curriculum.github.io/cat-api/cats.json")
+    .then((response) => response.json())
+    .then((data) => data.images);
+});
 
 // Reducer
 const initialState = {
@@ -36,38 +15,30 @@ const initialState = {
   status: "idle", // loading state
 };
 
-function catsReducer(state = initialState, action) {
-  switch (action.type) {
-    // sync actions
-    case "cats/catAdded":
-      return {
-        ...state,
-        entities: [...state.entities, action.payload],
-      };
-    case "cats/catUpdated":
-      return {
-        ...state,
-        entities: state.entities.map((cat) =>
-          cat.id === action.payload.id ? action.payload : cat
-        ),
-      };
-
-    // async actions
-    case "cats/fetchCats/pending":
-      return {
-        ...state,
-        status: "loading",
-      };
-    case "cats/fetchCats/fulfilled":
-      return {
-        ...state,
-        entities: action.payload,
-        status: "idle",
-      };
-
-    default:
-      return state;
+const catsSlice = createSlice({
+  name: "cats",
+  initialState,
+  reducers: {
+    catAdded(state, action) {
+      // using createSlice lets us mutate state!
+      state.entities.push(action.payload);
+    },
+    catUpdated(state, action) {
+      const cat = state.entities.find((cat) => cat.id === action.payload.id);
+      cat.url = action.payload.url;
+    },
+  },
+  extraReducers: {
+    // handle async action types
+    [fetchCats.pending](state){
+      state.status = "loading";
+    },
+    [fetchCats.fulfilled](state, action){
+      state.entities = action.payload;
+      state.status = "idle";
+    }
   }
-}
+});
 
-export default catsReducer;
+export const { catAdded, catUpdated } = catsSlice.actions;
+export default catsSlice.reducer;
